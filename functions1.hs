f0e04730a87d28ee5fcaabf759f543f4d72c5adb
Pentti Laitinen <cykyter@gmail.com>
2014-09-14 16:57:32 +0300

    triangles

--- functions1.hs
+++ functions1.hs
@@ -32,3 +32,19 @@ monthsTillMonnies goal income expenses = ceiling nonroundup
 -- == {- ceiling 0,903614458 -}
 -- 1
 
+
+calcTriangleArea :: Double -> Double -> Double -> Double
+calcTriangleArea a b c = area
+	where
+	area 	= sqrt (s*(s-a)*(s-b)*(s-c))
+	s 	= (a+b+c)/2
+
+-- Example
+-- calcTriangleArea 3 4 5 = 6.0
+-- calcTriangleArea 10 10 5 = 24.206145913796355
+
+-- calcTriangleArea 3 4 5
+-- == {- Resolve s = (3+4+5)/2 = 6 -}
+-- sqrt (s*(s-a)*(s-b)*(s-c))
+-- == {- Resolve area (6*(6-3)*(6-4)*(6-5)) -}
+-- 6.0
